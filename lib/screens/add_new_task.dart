81fc4cc821c5f65f916d67c43b9f20c389540108
farukyucel5 <farukyucel5@gmail.com>
2023-11-25 20:15:37 +0300

    close buttom has been added into the Add a new task page

--- lib/screens/add_new_task.dart
+++ lib/screens/add_new_task.dart
@@ -5,8 +5,47 @@ class AddNewTaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("This is the new page"),
+    double deviceHeight=MediaQuery.of(context).size.height;
+    double deviceWidth=MediaQuery.of(context).size.width;
+    return   SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              width: deviceWidth,
+              height: deviceHeight/10,
+              color: Colors.purple,
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        size: 40,
+                        color: Colors.white,
+                      )
+                  ),
+                  const Expanded(
+                      child: Text(
+                          "Add a new task",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      )
+
+                  )
+                ],
+              ),
+            ),
+
+          ],
+        )
+      ),
     );
   }
 }
